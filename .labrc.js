f92113569e472a5f26f6e69b693ae123e62f1046
feugy <damien.feugas@gmail.com>
2016-07-13 14:48:19 +0200

    Update some dependencies, use latest lab configuration file

--- .labrc.js
+++ .labrc.js
@@ -1,7 +1,20 @@
+const outputs = [{
+  reporter: 'console',
+  output: 'stdout'
+}, {
+  reporter: 'html',
+  output: 'coverage/index.html'
+}]
+
 module.exports = {
   coverage: true,
+  leaks: true,
+  globals: '__core-js_shared__', // came from power-assert
   lint: true,
-  transform: require('lab-espower-transformer'),
-  // equivalent of verbose
-  progress: 2
+  'lint-warnings-threshold': 10,
+  threshold: 98,
+  transform: './node_modules/lab-espower-transformer',
+  verbose: true,
+  reporter: outputs.map(o => o.reporter),
+  output: outputs.map(o => o.output)
 }
